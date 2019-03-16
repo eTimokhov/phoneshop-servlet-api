2655f1da37fd4316f38186fe4136f1f513df6ec3
eTimokhov <47916917+eTimokhov@users.noreply.github.com>
2019-03-16 16:31:02 +0300

    Task 1.5 Implemented ProductDao

--- src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
@@ -1,25 +1,35 @@
 package com.es.phoneshop.model.product;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class ArrayListProductDao implements ProductDao {
+
+    private List<Product> products = new ArrayList<>();
+
     @Override
-    public Product getProduct(Long id) {
-        throw new RuntimeException("Not implemented");
+    public synchronized Product getProduct(Long id) {
+        Optional<Product> product = products.stream().filter((p) -> p.getId().equals(id)).findFirst();
+        return product.orElse(null);
     }
 
     @Override
-    public List<Product> findProducts() {
-        throw new RuntimeException("Not implemented");
+    public synchronized List<Product> findProducts() {
+        return products.stream().filter((p) -> p.getPrice() != null && p.getStock() > 0)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public void save(Product product) {
-        throw new RuntimeException("Not implemented");
+    public synchronized void save(Product product) {
+        if (product != null && products.stream().noneMatch((p) -> p.getId().equals(product.getId())))
+            products.add(product);
     }
 
     @Override
-    public void delete(Long id) {
-        throw new RuntimeException("Not implemented");
+    public synchronized void delete(Long id) {
+        products.removeIf((p) -> p.getId().equals(id));
     }
 }
--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -3,7 +3,9 @@ package com.es.phoneshop.model.product;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.math.BigDecimal;
+
+import static org.junit.Assert.*;
 
 public class ArrayListProductDaoTest
 {
@@ -17,5 +19,32 @@ public class ArrayListProductDaoTest
     @Test
     public void testFindProductsNoResults() {
         assertTrue(productDao.findProducts().isEmpty());
+    }
+
+    @Test
+    public void testFindProductsWithResults() {
+        Product product = new Product();
+        product.setId(1L);
+        product.setPrice(BigDecimal.ONE);
+        product.setStock(1);
+        productDao.save(product);
+        assertEquals(1, productDao.findProducts().size());
+    }
+
+    @Test
+    public void testGetProduct() {
+        Product product = new Product();
+        product.setId(1L);
+        productDao.save(product);
+        assertNotNull(productDao.getProduct(1L));
+    }
+
+    @Test
+    public void testDeleteProduct() {
+        Product product = new Product();
+        product.setId(1L);
+        productDao.save(product);
+        productDao.delete(1L);
+        assertNull(productDao.getProduct(1L));
     }
 }
