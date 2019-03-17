4c43e419a8c12685a41eb65f8cbda070634d6ced
eTimokhov <47916917+eTimokhov@users.noreply.github.com>
2019-03-17 11:53:41 +0300

    Добавил кейсы в ArrayListProductDaoTest

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -47,4 +47,35 @@ public class ArrayListProductDaoTest
         productDao.delete(1L);
         assertNull(productDao.getProduct(1L));
     }
+
+    @Test
+    public void testAddTwoProductsWithSameId() {
+        Product product1 = new Product();
+        product1.setId(1L);
+        product1.setPrice(BigDecimal.ONE);
+        product1.setStock(1);
+        productDao.save(product1);
+        Product product2 = new Product();
+        product2.setId(1L);
+        product2.setPrice(BigDecimal.ONE);
+        product2.setStock(1);
+        productDao.save(product2);
+        assertEquals(1, productDao.findProducts().size());
+    }
+
+    @Test
+    public void testFindProductsWithZeroStock() {
+        Product product = new Product();
+        product.setPrice(BigDecimal.ONE);
+        productDao.save(product);
+        assertEquals(0, productDao.findProducts().size());
+    }
+
+    @Test
+    public void testFindProductsWithNullPrice() {
+        Product product = new Product();
+        product.setStock(1);
+        productDao.save(product);
+        assertEquals(0, productDao.findProducts().size());
+    }
 }
