8e3649ba5aa6bf899c3c4317116e35b3d74215cc
eTimokhov <47916917+eTimokhov@users.noreply.github.com>
2019-03-25 12:19:38 +0300

    Fixed ProductDemodataServletContextListener

--- src/test/java/com/es/phoneshop/web/ProductDemodataServletContextListenerTest.java
+++ src/test/java/com/es/phoneshop/web/ProductDemodataServletContextListenerTest.java
@@ -1,14 +1,33 @@
 package com.es.phoneshop.web;
 
 import com.es.phoneshop.model.product.ArrayListProductDao;
+import com.es.phoneshop.model.product.ProductDao;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.assertNotEquals;
 
 public class ProductDemodataServletContextListenerTest {
 
+    private ArrayListProductDao productDao = ArrayListProductDao.getInstance();
+    private ProductDemodataServletContextListener listener = new ProductDemodataServletContextListener();
+
+    @Before
+    public void setup() {
+        productDao.setProducts(new ArrayList<>());
+        listener.contextInitialized(null);
+    }
+
+    @After
+    public void clearDao() {
+        productDao.setProducts(new ArrayList<>());
+    }
+
     @Test
     public void testAddProductsToDao() {
-        assertNotEquals(0, ArrayListProductDao.getInstance().findProducts());
+        assertNotEquals(0, productDao.findProducts().size());
     }
 }
