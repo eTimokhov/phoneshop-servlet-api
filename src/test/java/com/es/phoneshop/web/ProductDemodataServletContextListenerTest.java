273e51c0e3c25bcb7de2855d7a41b15fc514e17d
eTimokhov <47916917+eTimokhov@users.noreply.github.com>
2019-03-29 23:32:56 +0300

    Code cleanup

--- src/test/java/com/es/phoneshop/web/ProductDemodataServletContextListenerTest.java
+++ src/test/java/com/es/phoneshop/web/ProductDemodataServletContextListenerTest.java
@@ -1,7 +1,6 @@
 package com.es.phoneshop.web;
 
 import com.es.phoneshop.model.product.ArrayListProductDao;
-import com.es.phoneshop.model.product.ProductDao;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
