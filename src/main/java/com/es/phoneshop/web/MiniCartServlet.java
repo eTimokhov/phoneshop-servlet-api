9f7ec83da47680ec99aeebe4ad1ab88f2fcef585
eTimokhov <47916917+eTimokhov@users.noreply.github.com>
2019-04-04 21:51:12 +0300

    Task 4.4 Implemented MiniCart

---
+++ src/main/java/com/es/phoneshop/web/MiniCartServlet.java
@@ -0,0 +1,30 @@
+package com.es.phoneshop.web;
+
+import com.es.phoneshop.model.cart.CartService;
+import com.es.phoneshop.model.cart.HttpSessionCartService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MiniCartServlet extends HttpServlet {
+
+    private CartService cartService;
+
+    @Override
+    public void init() {
+        cartService = HttpSessionCartService.getInstance();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("cart", cartService.getCart(request));
+        request.getRequestDispatcher("/WEB-INF/fragments/miniCart.jsp").include(request, response);
+    }
+
+    public void setCartService(CartService cartService) {
+        this.cartService = cartService;
+    }
+}
---
+++ src/main/webapp/WEB-INF/fragments/miniCart.jsp
@@ -0,0 +1,22 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<jsp:useBean id="cart" class="com.es.phoneshop.model.cart.Cart" scope="request" />
+
+<h3>Cart</h3>
+<table>
+    <c:forEach var="cartItem" items="${cart.cartItems}">
+        <tr>
+            <td>
+                <img src="${cartItem.product.imageUrl}" width="40">
+            </td>
+            <td>
+                ${cartItem.quantity}
+            </td>
+        </tr>
+    </c:forEach>
+    <tr>
+        <td>Total</td>
+        <td>${cart.totalPrice}</td>
+    </tr>
+</table>
